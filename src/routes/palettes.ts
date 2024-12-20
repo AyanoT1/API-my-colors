d503afb3e415762b46addf727ae4e51ff98bcc0f
Sergio Romero Veliz <128757548+AyanoT1@users.noreply.github.com>
2024-12-19 21:16:56 -0300

    feat: added /api/id/:id endpoint

--- src/routes/palettes.ts
+++ src/routes/palettes.ts
@@ -1,10 +1,19 @@
 import express from 'express'
-import { getAllPalettes } from '../services/paletteServices'
+import { getAllPalettes, getPaletteById } from '../services/paletteServices'
 
 const router = express.Router()
 
 router.get('/', (_, res) => {
   res.send(getAllPalettes())
+})
+
+router.get('/id/:id', (req, res) => {
+  const palette = getPaletteById(+req.params.id)
+  if (palette !== undefined) {
+    res.send(palette)
+  } else {
+    res.status(400).send({ error: 'Palette not found' })
+  }
 })
 
 export default router
--- src/services/paletteServices.ts
+++ src/services/paletteServices.ts
@@ -4,3 +4,8 @@ import palettes from './colorsData.json'
 const allPalettes: ColorPalette[] = palettes as ColorPalette[]
 
 export const getAllPalettes = (): ColorPalette[] => allPalettes
+
+export function getPaletteById (id: number): ColorPalette | undefined {
+  const result = allPalettes.find(d => d.id === id)
+  return (result != null) ? result : undefined
+}
